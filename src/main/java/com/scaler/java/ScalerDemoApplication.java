21b5d63325d35719dd4fea070e39f7ca87cff90a
Nikhil-Dev <hi.nikhil25@gmail.com>
2024-05-08 10:21:35 +0530

    scaler demo first project

---
+++ src/main/java/com/scaler/java/ScalerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerDemoApplication.class, args);
+    }
+
+}
